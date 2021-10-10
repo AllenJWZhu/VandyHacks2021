c85e9610a32b87154af3b8d167dac5c984c62f25
Jiawei Zhu <allenzhu724@gmail.com>
2021-10-09 19:06:46 -0500

    added file static

--- template/login.js
+++ template/login.js
@@ -13,12 +13,10 @@ function login() {
         alert("Please enter your password");
  
     } else if(username.value == "admin" && password.value == "123456") {
-        //若复选框勾选,则添加Cookie
         if (isRmbPwd == true) {   
             setCookie ("This is username", username.value.trim(), 7);
             setCookie (username.value.trim(), password.value.trim(), 7);
         }
-        //否则清除Cookie
         else {
             delCookie ("This is username");
             delCookie (username.value.trim());
